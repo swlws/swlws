3b2e755c6ac647c4cb1e11ac4628c0888fa09847
swlws <979623074@qq.com>
2018-09-17 20:33:21 +0800

    resume base info

---
+++ app/config/resumeinfo.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const resume = {
+  baseInfo: {
+    name: '孙文龙',
+    sex: '男',
+    education: '本科 / 西南科技大学 / 计算机科学与技术学院 / 软件工程',
+    job: '前端工程师'
+  }
+}
+
+export default resume;
--- app/pages/resume.js
+++ app/pages/resume.js
@@ -1,13 +1,76 @@
 'use strict'
 
+// load opensource code
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-export default class Resume extends React.Component{
-    constructor(){
-        super();
-    }
+// load self code
+import css from './../css/resume.css';
 
-    render(){
-        return <div>resume</div>
+// load config
+import resumeInfo from './../config/resumeinfo';
+console.log(resumeInfo)
+
+// global variable
+import meImg from './../images/me.jpg';
+
+class BaseInfo extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentWillMount() {
+    console.log('baseInfo', this.props.baseInfo)
+    this.baseInfo = this.props.baseInfo || {};
+  }
+
+  componentDidMount() {
+    this.loadMePhotoInfo();
+  }
+
+  loadMePhotoInfo() {
+    let cls = ReactDOM.findDOMNode(this).getElementsByClassName('resume_me_photo');
+    if (cls && cls.length > 0) {
+      let img = new Image();
+      img.src = meImg;
+
+      let baseInfoBox = cls[0];
+      baseInfoBox.appendChild(img);
     }
+  }
+
+  render() {
+    const baseInfo = this.baseInfo;
+    return (
+      <div className="resume_base_info border_bottom">
+        <div className="resume_me_photo"></div>
+        <div>
+          <p id="name_and_title_box">
+            <span>{baseInfo.name}</span>
+            <span>{baseInfo.sex}</span>
+          </p>
+          <p>
+            <label>教育经历</label><span>{baseInfo.education}</span>
+          </p>
+          <p>
+            <label>期望职位</label><span>{baseInfo.job}</span>
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default class Resume extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <BaseInfo baseInfo={resumeInfo.baseInfo} />
+      </div>
+    )
+  }
 }
